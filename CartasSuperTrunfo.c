b4a737cc1da9e367a43eb71bd0ced0499df65b13
Juan Sued <juansued19@gmail.com>
2025-02-22 09:35:30 -0300

    test/ok

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,11 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    //teste commit 
     return 0;
 }
+
+
+
+
+
